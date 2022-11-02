34eef0a189d9986f2ddbe103b5a8da8d4ce4a5ab
khushipo <dheerajpoddar6@gmail.com>
2022-11-02 14:33:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import Weather from "./weather"
+import Heading from "./heading"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  <>
+    <Weather/>
+    <Heading/>
+  <h2>jai mata dii</h2>
+
+  </>
   );
 }
 
---
+++ src/heading.js
@@ -0,0 +1,11 @@
+import "./App.css"
+function Heading(){
+    return (<>
+   <div className="main">
+    <h2 className="head">jai mata dii</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum dicta fugit vitae pariatur deleniti adipisci mollitia obcaecati inventore reiciendis aperiam, distinctio, dolore amet sunt nobis veniam. Totam sunt praesentium vero magnam accusamus consequuntur reprehenderit maxime quisquam, libero delectus assumenda magni, expedita cupiditate nesciunt qui placeat facilis. Eum ex repellendus alias, quidem, hic omnis sapiente modi voluptatibus rerum rem perspiciatis delectus repellat architecto natus voluptates nemo odio dolores eligendi? Veritatis voluptates placeat voluptate excepturi reiciendis, aperiam illo sit tempora voluptatem blanditiis tempore aut harum possimus, quia atque? Animi, quae provident obcaecati iusto repudiandae, a iure consequuntur dignissimos, et error similique ut?</p>
+    </div>
+    </>)
+}
+
+export default Heading;
